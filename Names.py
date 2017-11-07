572bc18f965ec49736fc2d3fea40aaab186f8e95
Cani <tuannguyen0115@gmail.com>
2017-11-07 13:52:45 -0500

    commit Names

---
+++ Names.py
@@ -0,0 +1,29 @@
+print "Part 1"
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+for items in students:
+    print " ".join(items.values())  
+
+print "\nPart 2"
+users = {
+ 'Students': [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'},
+     {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+     {'first_name' : 'KB', 'last_name' : 'Tonel'}
+  ],
+ 'Instructors': [
+     {'first_name' : 'Michael', 'last_name' : 'Choi'},
+     {'first_name' : 'Martin', 'last_name' : 'Puryear'}
+  ]
+ }
+
+for key, data in users.items():
+    print key
+    for index, i in enumerate(data,1):
+        name = " ".join(i.values())                    
+        print "{} - {} - {}".format(index,name.upper(),len(name)+1-len(i.values()))
