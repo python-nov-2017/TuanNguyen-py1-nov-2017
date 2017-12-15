da5a378137722c6b4c739b151b2f3d8a5bcdd682
Cani <tuannguyen0115@gmail.com>
2017-12-15 18:13:48 +0900

    commit Various

---
+++ Django/DjangoApp/apps/user_login/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    update_at = models.DateTimeField(auto_now = True)
+    def __repr__(self):
+        return "<Blog object: name: {} {} ".format(self.first_name, self.last_name)
