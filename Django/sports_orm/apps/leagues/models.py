e856277bb462361a2c05f79c4eb94633cdfed084
Cani <tuannguyen0115@gmail.com>
2017-12-16 09:02:18 +0900

    commit SportORM2

--- Django/sports_orm/apps/leagues/models.py
+++ Django/sports_orm/apps/leagues/models.py
@@ -5,14 +5,19 @@ class League(models.Model):
 	sport = models.CharField(max_length=15)
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
-
+	def __repr__(self):
+		return "<League Object: {}>".format(self.name)
 class Team(models.Model):
 	location = models.CharField(max_length=50)
 	team_name = models.CharField(max_length=50)
 	league = models.ForeignKey(League, related_name="teams")
+	def __repr__(self):
+		return "<Team Object: {}>".format(self.team_name)
 
 class Player(models.Model):
 	first_name = models.CharField(max_length=15)
 	last_name = models.CharField(max_length=15)
 	curr_team = models.ForeignKey(Team, related_name="curr_players")
 	all_teams = models.ManyToManyField(Team, related_name="all_players")
+	def __repr__(self):
+		return "<Player Object: {} {}>".format(self.first_name, self.last_name)
--- Django/sports_orm/apps/leagues/views.py
+++ Django/sports_orm/apps/leagues/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import League, Team, Player
+from django.db.models import Count
 
 from . import team_maker
 
@@ -22,6 +23,21 @@ def index(request):
 		"p3": Player.objects.filter(last_name="Cooper").exclude(first_name="Joshua"),
 		"p4": Player.objects.filter(first_name="Alexander")|Player.objects.filter(first_name="Wyatt"),
 
+		"t21": League.objects.get(name="Atlantic Soccer Conference").teams.all(),
+		"p21": Team.objects.get(team_name="Penguins", location="Boston").curr_players.all(),
+		"p22": Player.objects.filter(curr_team__league__name="International Collegiate Baseball Conference"),
+		"p23": Player.objects.filter(curr_team__league__name="American Conference of Amateur Football").filter(last_name="Lopez"),
+		"p24": Player.objects.filter(curr_team__league__sport="Football"),
+		"t22": Team.objects.filter(curr_players__first_name="Sophia"),
+		"l21": League.objects.filter(teams__curr_players__first_name="Sophia"),
+		"p25": Player.objects.filter(last_name="Flores").exclude(curr_team__team_name="Roughriders"),
+		"t23": Team.objects.filter(all_players__first_name="Samuel", all_players__last_name="Evans"),
+		"p26": Player.objects.filter(all_teams__team_name="Tiger-Cats"),
+		"p27": Player.objects.filter(all_teams__team_name="Vikings", all_teams__location="Wichita").exclude(curr_team__team_name="Vikings", curr_team__location="Wichita"),
+		"t24": Team.objects.filter(all_players__first_name="Jacob", all_players__last_name="Gray").exclude(location="Oregon"),
+		"p28": Player.objects.filter(all_teams__league__name="Atlantic Federation of Amateur Baseball Players").filter(first_name="Joshua"),
+		"t25": Team.objects.annotate(player_count=Count('all_players')).filter(player_count__gte=12),
+		"p29": Player.objects.annotate(team_count=Count('all_teams')).order_by("team_count"),
 		"leagues": League.objects.all(),
 		"teams": Team.objects.all(),
 		"players": Player.objects.all(),
