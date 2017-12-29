0721076f9db51001d29a266afe39a85e0f4f4d94
Cani <tuannguyen0115@gmail.com>
2017-12-29 15:14:50 -0500

    Commit Ajax Pagination

--- DjangoAjax/apps/pagination/views.py
+++ DjangoAjax/apps/pagination/views.py
@@ -17,12 +17,10 @@ def add_lead(request):
     form = AddUserForm(request.POST)
     if form.is_valid():
         form.save()
-    print "add_lead"
     return redirect('/pagination')
 
 def get_leads(request=None):
     leads = User.objects.all()
-    print request.POST['page']
     if any(x in ['name','from_date','to_date'] for x in request.POST):
         if request.POST['name'] != "":
             leads = leads.filter(first_name__startswith=request.POST['name'])
@@ -31,13 +29,8 @@ def get_leads(request=None):
         if request.POST['to_date'] != "":
             leads = leads.filter(created_at__lt=request.POST['to_date'])
     p = Paginator(leads, LEADS_PER_PAGE)
-    print "p =", p
     pages = range(p.num_pages)
-    print "pages = ", pages
     pages = [x+1 for x in pages]
-    print "pages = ", pages
     subset = p.page(request.POST['page'])
-    print "subset = ", subset
     leads = subset.object_list  
-    print "leads = ", leads  
     return render(request, 'pagination/user_table.html', { "leads": leads, "pages":pages })
